cbb4c3bf14b0e625b345f02e95e9fa6e0414ac75
amtenuJava <java.amanuel@gmail.com>
2023-06-05 10:01:28 -0600

    List implementation , updating parameter

--- src/dev/aman/ListItem.java
+++ src/dev/aman/ListItem.java
@@ -1,30 +1,27 @@
 package dev.aman;
 
-import java.util.List;
 
-public abstract   class ListItem {
 
-    protected ListItem rightItem;
-    protected ListItem leftItem;
-
+public abstract class ListItem {
+    // write code here
+    protected ListItem rightLink, leftLink;
     protected Object value;
 
-    public ListItem(Object value) {
+    public ListItem(Object value){
         this.value = value;
     }
 
     abstract ListItem next();
-    abstract ListItem setNext();
+    abstract ListItem setNext(ListItem rightLink);
     abstract ListItem previous();
-    abstract ListItem setPrevious();
-    abstract ListItem compareTo();
+    abstract ListItem setPrevious(ListItem leftLink);
+    abstract int compareTo(ListItem item);
 
     public Object getValue(){
         return value;
     }
 
     public void setValue(Object value){
-        this.value=value;
+        this.value = value;
     }
-
 }
